57e18578cce093350ebe7c5c07df6dcd37a2f5fe
arif <arif-celik56@hotmail.com>
2023-05-22 14:23:53 +0300

    configuration and README update

--- README.md
+++ README.md
@@ -1,19 +1,12 @@
 # Nuxt 3 Minimal Starter
 
-Look at the [Nuxt 3 documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
+Nuxt demo application
 
 ## Setup
 
 Make sure to install the dependencies:
 
 ```bash
-# yarn
-yarn install
-
-# npm
-npm install
-
-# pnpm
 pnpm install
 ```
 
@@ -32,11 +25,3 @@ Build the application for production:
 ```bash
 npm run build
 ```
-
-Locally preview production build:
-
-```bash
-npm run preview
-```
-
-Check out the [deployment documentation](https://nuxt.com/docs/getting-started/deployment) for more information.
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,33 @@
-import tailwindTypography from '@tailwindcss/typography'
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: 'src',
+  // @ts-ignore
+  devtools: true,
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      titleTemplate: "Nuxt Demo - %s",
+      meta: [
+        { name: 'description', content: 'Nuxt 3 demo app' }
+      ]
+    }
+  },
   css: ['@/assets/css/style.css'],
-  modules: ['@nuxtjs/tailwindcss'],
   router: {
     options: {
       linkActiveClass: 'active',
       linkExactActiveClass: 'exact-active'
     }
   },
-  tailwindcss: {
-    // @ts-ignore
-    config: {
-      plugins: [tailwindTypography]
+  runtimeConfig: {
+    public: {
+      'API_URL': '',
     }
+  },
+  // experimental: {
+  //   typedPages: true
+  // },
+  vite: {
+    publicDir: '../public'
   }
 })
